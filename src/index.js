2c3c2335fd0cdf4f3e6953b49865849dca512323
Eric Martins <eric.martins@radixeng.com.br>
2021-03-10 19:36:34 -0300

    checks if id is a uuid and if todo belongs to a specific user

--- src/index.js
+++ src/index.js
@@ -34,11 +34,47 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  const idIsValid = validate(id);
+
+  if(!idIsValid){
+    return response.status(400).json({ error: 'Id is not a uuid!' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
+
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
